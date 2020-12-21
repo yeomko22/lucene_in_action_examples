915fac8b53317d7f232558dc6a231b769a2f6a05
hyeongjun.kim <hyeongjun.kim@navercorp.com>
2020-12-21 20:30:14 +0900

    ch1. meetlucene example codes

---
+++ src/main/java/ch1_meetlucene/Indexer.java
@@ -0,0 +1,86 @@
+package ch1_meetlucene;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.*;
+import java.nio.file.Paths;
+
+public class Indexer {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Usage: java " + Indexer.class.getName() +
+                    " <index dir> <data dir>");
+        }
+
+        String indexDir = args[0];
+        String dataDir = args[1];
+
+        long start = System.currentTimeMillis();
+        Indexer indexer = new Indexer(indexDir);
+        int numIndexed;
+
+        try {
+            numIndexed = indexer.index(dataDir, new TextFilesFilter());
+        } finally {
+            indexer.close();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("indexing " + numIndexed + " files took " + (end - start) + " milliseconds");
+    }
+
+    private IndexWriter writer;
+
+    public Indexer(String indexDir) throws IOException {
+        Directory dir = FSDirectory.open(Paths.get(indexDir));
+        Analyzer analyzer = new StandardAnalyzer();
+
+        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+        iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+
+        writer = new IndexWriter(dir, iwc);
+    }
+
+    public void close() throws IOException {
+        writer.close();
+    }
+
+    public int index(String dataDir, FileFilter filter) throws Exception {
+        File[] files = new File(dataDir).listFiles();
+        for (File f: files) {
+            if (!f.isDirectory() && !f.isHidden() && f.exists() && f.canRead() && (filter == null || filter.accept(f))) {
+               indexFile(f);
+            }
+        }
+        return writer.getDocStats().numDocs;
+    }
+
+    private static class TextFilesFilter implements FileFilter {
+        @Override
+        public boolean accept(File path) {
+            return path.getName().toLowerCase().endsWith(".txt");
+        }
+    }
+
+    protected Document getDocument(File f) throws Exception {
+        Document doc = new Document();
+        doc.add(new TextField("contents", new BufferedReader(new FileReader(f))));
+        doc.add(new StringField("filename", f.getName(), Field.Store.YES));
+        doc.add(new StringField("fullpath", f.getCanonicalPath(), Field.Store.YES));
+        return doc;
+    }
+
+    private void indexFile(File f) throws Exception {
+        System.out.println("Indexing " + f.getCanonicalPath());
+        Document doc = getDocument(f);
+        writer.addDocument(doc);
+    }
+}
---
+++ src/main/java/ch1_meetlucene/readme.md
@@ -0,0 +1,15 @@
+## ch1. meet lucene
+
+### major changes
+- creating IndexWriter is changed.
+- creating IndexReader and IndexSearcher is changed.  
+- IndexSearcher.close() is removed.
+
+### run configuration 
+- working directory: {PROJECT_PATH}/src/main/java/ch1_meetlucene
+  
+### run index
+- ch1_meetlucene.Indexer index data
+  
+### run search
+- ch1_meetlucene.Searcher index patent
